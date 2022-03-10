bc027f203208dfb300d11b7bb3e51da03d30aefb
Alozious <aloziousmusiime@gmail.com>
2022-03-10 21:01:01 +0300

    new

--- mda/lib/main.dart
+++ mda/lib/main.dart
@@ -7,7 +7,7 @@ void main(List<String> args) {
   runApp(const MainApp());
 }
 
-// aziz comments
+// aloyzious comments
 class MainApp extends StatefulWidget {
   const MainApp({Key? key}) : super(key: key);
 
